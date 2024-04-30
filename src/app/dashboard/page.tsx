b2335da8eb2c1d593837a8f1cbba959ace5eedcf
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-04-30 09:51:15 +0900

    ダッシュボードのUIを実装

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Appbar from "@/components/appbar/Appbar";
+
+export default function DashBoard() {
+
+	return (
+		<Appbar>
+			<h1 style={{fontSize: "40px"}}>dashboard</h1>
+		</Appbar>
+	)
+};
