da11c27cd1e8108221ba24ae57af62b68b624d8a
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-05-02 13:40:06 +0900

    軽く修正

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,7 +1,35 @@
-export default function Board() {
+import RecipeReviewCard from "@/core/card/card";
+import { CardType } from "@/types/card.type";
+
+const tweets: CardType[] = [
+  {
+    id: 1,
+    user: {
+      name: "Alice",
+      stuNumber: "123456",
+    },
+    text: "Hello World!",
+    createdAt: "2021-10-01",
+  },
+  {
+    id: 2,
+    user: {
+      name: "Bob",
+      stuNumber: "654321",
+    },
+    text: "Hi Alice!",
+    createdAt: "2021-10-02",
+  },
+];
+
+const Timeline = () => {
   return (
     <div>
-      <p>Board</p>
+      {tweets.map((tweet, index) => (
+        <RecipeReviewCard card={tweet} key={index} />
+      ))}
     </div>
   );
-}
+};
+
+export default Timeline;
